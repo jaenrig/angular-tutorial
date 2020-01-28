9ffe4f5f4bb1d038fa49614a7764751af0af2cc8
jaenrig <jaenrig@gmail.com>
2020-01-28 22:36:12 +0100

    shipping pricing components

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'})
-  
+
 export class CartService {
   items = [];
 
@@ -19,6 +20,12 @@ export class CartService {
     return this.items;
   }
 
-  constructor() { }
+   getShippingPrices() {
+    return this.http.get('/assets/shipping.json');
+  }
+
+  constructor(
+    private http: HttpClient
+  ) { }
 
 }
